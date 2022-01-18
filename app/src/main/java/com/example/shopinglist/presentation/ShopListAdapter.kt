7dd6bbc838ee15dc71161d7de4670a4dfc5cf371
Livec <livecyura@gmail.com>
2022-01-18 14:17:16 +0200

    Task_2_3

---
+++ app/src/main/java/com/example/shopinglist/presentation/ShopListAdapter.kt
@@ -0,0 +1,49 @@
+package com.example.shopinglist.presentation
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.shopinglist.R
+import com.example.shopinglist.domain.ShopItem
+
+class ShopListAdapter: RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>() {
+
+    var shopList = listOf<ShopItem>()
+    set(value){
+        field = value
+        notifyDataSetChanged()
+    }
+
+    class ShopItemViewHolder(val view: View): RecyclerView.ViewHolder(view){
+
+        val tvName = view.findViewById<TextView>(R.id.tv_name)
+        val tvCount = view.findViewById<TextView>(R.id.tv_count)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(
+            R.layout.item_shop_enabled, parent, false)
+        return ShopItemViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ShopItemViewHolder, position: Int) {
+        val shopItem = shopList[position]
+        val status = if(shopItem.enabled){
+            "Active"
+        }else{
+            "Not active"
+        }
+        holder.tvName.text = shopItem.name
+        holder.tvCount.text = shopItem.count.toString()
+        holder.view.setOnLongClickListener{
+            true
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return shopList.size
+    }
+
+}
