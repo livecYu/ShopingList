03f014b1a53c11b7e8a05b2e6f760f4515d490a6
Livec <livecyura@gmail.com>
2022-01-24 14:47:42 +0200

    Task_3_3

--- app/src/main/java/com/example/shopinglist/presentation/ShopItemActivity.kt
+++ app/src/main/java/com/example/shopinglist/presentation/ShopItemActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import com.example.shopinglist.R
 
 class ShopItemActivity : AppCompatActivity() {
+    private lateinit var viewModel: ShopItemViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop_item)
